b584646df65d355dfec6065a02b81c771f170cf8
vikranth1997 <vikranth1997@gmail.com>
2017-07-18 02:41:32 +0530

    minor bug fix in development.js

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -7,7 +7,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://'+ process.env.DEV_USER + ':' + process.env.DEV_PASSWORD + '@localhost/caportal-dev'
+    uri: 'mongodb://localhost/caportal-dev'
   },
 
   // Seed database on startup
